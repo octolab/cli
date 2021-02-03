150475deb9e75574605b68f4984b774194212462
Kamil Samigullin <kamil@samigullin.info>
2021-02-03 09:05:10 +0300

    refactor Silent error

--- errors/silent.go
+++ errors/silent.go
@@ -1,9 +1,37 @@
 package errors
 
+import "strings"
+
 // Silent holds a root cause error and allows to exit
 // with specified code and optionally with a custom message.
-type Silent struct {
+type Silent interface {
 	error
-	Code    int
-	Message string
+	Code() int
+	Message() (string, bool)
+}
+
+func NewSilent(cause error, code int, message ...string) Silent {
+	return silent{cause, code, strings.Join(message, ": ")}
+}
+
+type silent struct {
+	error
+	code    int
+	message string
+}
+
+func (err silent) Cause() error {
+	return err.error
+}
+
+func (err silent) Code() int {
+	return err.code
+}
+
+func (err silent) Message() (string, bool) {
+	return err.message, err.message != ""
+}
+
+func (err silent) Unwrap() error {
+	return err.error
 }
---
+++ errors/silent_test.go
@@ -0,0 +1,7 @@
+package errors_test
+
+import "testing"
+
+func TestSilent(t *testing.T) {
+	t.SkipNow()
+}
